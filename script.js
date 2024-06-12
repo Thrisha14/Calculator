aa10f3bb96349becef85a7af15f963ac37fc7e29
Thrisha <sreemaravichandran1405@gmail.com>
2024-06-12 06:48:23 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById('result').value = '';
+  }
+  
+  function calculateResult() {
+    try {
+      var result = eval(document.getElementById('result').value);
+      document.getElementById('result').value = result;
+    } catch (e) {
+      document.getElementById('result').value = 'Error';
+    }
+  }
+  
